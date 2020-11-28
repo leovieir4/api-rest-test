1d1b55a120f328a642d996512e4ffdc920e419b5
leovieir4 <leovieiradevjs@gmail.com>
2020-11-28 20:42:54 -0300

    feat: ensure DbAuthentication calls UpdateAccessRepositoryWithCorrect values

--- src/domain/usecases/authentication/db-authentication.ts
+++ src/domain/usecases/authentication/db-authentication.ts
@@ -2,15 +2,17 @@ import { HashComparer } from '../../../data/protocols/criptography/hash-comparer
 import { TokenGenerator } from '../../../data/protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-account-by-email-repository'
 import { Authentication, AuthenticationModel } from '../../usecases/authentication'
-
+import { UpdateAcessTokenRepository } from '../../../data/protocols/db/update-access-token-repository'
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+  private readonly updateAcessTokenRepository: UpdateAcessTokenRepository
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator, updateAcessTokenRepository: UpdateAcessTokenRepository) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAcessTokenRepository = updateAcessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<any> {
@@ -19,6 +21,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAcessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
     }
