182636051cdc92834a3b108e7e6a511851092e58
leovieir4 <leovieiradevjs@gmail.com>
2020-11-28 21:30:10 -0300

    refactor: move bcrypt adaptar files to a folder

---
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt'
+import { Encrypter } from '../../../data/protocols/criptography/encrypter'
+import { HashComparer } from '../../../data/protocols/criptography/hash-comparer'
+export class BcryptAdapter implements Encrypter, HashComparer {
+  private readonly salt: number
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt (value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
+  }
+}
