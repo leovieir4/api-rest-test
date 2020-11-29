0410e1dde7f5539689005b2709e7f7fcdfc1ac10
leovieir4 <leovieiradevjs@gmail.com>
2020-11-29 17:10:10 -0300

    feat: ensure investments routes return 403 if no accessToken

--- src/main/routes/investments/investments-routes.ts
+++ src/main/routes/investments/investments-routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
+import { adaptMiddleware } from '../../adapters/express-middleware-adapter copy'
 import { adaptRoute } from '../../adapters/express-route-adapter'
 import { makeInvestmentsController } from '../../factories/investments/investments-factory'
+import { makeAuthMiddleware } from '../../factories/middlewares/auth-middleware-factory'
 import { tickets } from './tickets'
 export default (router: Router): void => {
-  router.post('/investments', adaptRoute(makeInvestmentsController()))
-  router.get('/investments', adaptRoute(makeInvestmentsController()))
-  router.delete('/investments', adaptRoute(makeInvestmentsController()))
-  router.get('/investments/tickets', (req, res) => {
+  const adminAuth = adaptMiddleware(makeAuthMiddleware())
+
+  router.post('/investments', adminAuth, adaptRoute(makeInvestmentsController()))
+  router.get('/investments', adminAuth, adaptRoute(makeInvestmentsController()))
+  router.delete('/investments', adminAuth, adaptRoute(makeInvestmentsController()))
+  router.get('/investments/tickets', adminAuth, (req, res) => {
     return res.status(200).send(tickets)
   })
 }
