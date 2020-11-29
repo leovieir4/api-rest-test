d13d7505de7d8ddfe28c3cbda2cfed7338415ce1
leovieir4 <leovieiradevjs@gmail.com>
2020-11-29 00:56:46 -0300

    feat: create feature get acessToken

--- src/data/protocols/criptography/hasher.ts
+++ src/data/protocols/criptography/hasher.ts
@@ -1,3 +1,3 @@
 export interface Hasher {
-  hash (value: string)
+  hash (value: string): Promise<string>
 }
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/gorila-node-api',
   mongoTest: 'mongodb://localhost:27017/test',
-  port: process.env.port || 5050
+  port: process.env.port || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
