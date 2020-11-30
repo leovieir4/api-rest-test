d2ab8c54e835d3bd995c37ab9b48629c6e77a040
leovieir4 <leovieiradevjs@gmail.com>
2020-11-29 22:13:29 -0300

    refactor: alter port to herouku

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/gorila-node-api',
   mongoTest: 'mongodb://localhost:27017/test',
-  port: process.env.port,
+  port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
