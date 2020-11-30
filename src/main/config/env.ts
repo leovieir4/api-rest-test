25181676be257872e9646663bff84db5707177e5
leovieir4 <leovieiradevjs@gmail.com>
2020-11-29 22:06:02 -0300

    refactor: test to heroku

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/gorila-node-api',
-  mongoTest: 'mongodb://localhost:27017/test',
-  port: process.env.port || 5050,
+  mongoUrl: process.env.MONGO_URL,
+  port: process.env.port,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
